fb0a1a8f3827837f67b0ba5b2322076a971acd9b
LuigiToniolo <luigibtoniolo@gmail.com>
2022-10-17 13:59:17 -0300

    adicionando repoítório projeto crud

---
+++ WebApi/WebApi/Migrations/20221017132030_InitialCreate.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using MySql.EntityFrameworkCore.Metadata;
+
+namespace WebApi.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "DCandidate",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    fullName = table.Column<string>(type: "nvarchar(100)", nullable: true),
+                    mobile = table.Column<string>(type: "nvarchar(16)", nullable: true),
+                    email = table.Column<string>(type: "nvarchar(100)", nullable: true),
+                    age = table.Column<int>(type: "int", nullable: false),
+                    bloodgroup = table.Column<string>(type: "nvarchar(3)", nullable: true),
+                    address = table.Column<string>(type: "nvarchar(100)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DCandidate", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DCandidate");
+        }
+    }
+}
---
+++ WebApi/WebApi/Models/DonationDBContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApi.Models;
+
+namespace WebApi.Models
+{
+    public class DonationDBContext : DbContext
+    {
+        public DonationDBContext(DbContextOptions<DonationDBContext> options) : base(options)
+        {
+            
+        }
+        public DbSet<WebApi.Models.DCandidate> DCandidate { get; set; }
+
+        public DbSet<DCandidate> DCandidates { get; set; }
+    }
+}
